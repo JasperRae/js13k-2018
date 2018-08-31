00372081b38e79f654206c88465d071c6855c2e9
JasperRae <donavanlq97@yahoo.com>
2018-08-30 22:21:31 -0700

    added stat bars, flipped menu toggle, refactoring

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# js13k-2018
+# js13k-2018 To-Do
+
+* Where do I put the items
+* How do I manage numbers (purchasing, investing, saving, etc.)
+
+Energy and hunger dictates how much you can do, depletes based on activity (Work, Study, Create, Hobbies, etc.)
--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+function startUp() {
+    
+}
+
 function toggleMenu(id) {
     var menuWindow = document.getElementById(id+"-window");
     var getWindows = document.getElementsByClassName("menu-window");
     
-    if (menuWindow.style.display == "none") {
+    if (menuWindow.style.display == "block") {
+        menuWindow.style.display = "none";
+    } else {
         for (var i=0; i < getWindows.length; i++) {
             getWindows[i].style.display = "none";
         }
         menuWindow.style.display = "block";
-    } else {
-        menuWindow.style.display = "none";
     }
+}
+
+function updateStats(number, type) {
+    var energyBar = document.getElementById("energy-bar");
+    var hungerBar = document.getElementById("hunger-bar");
+    var barType = type.className;
+
+    if (barType == "food") {
+        hungerBar.value += parseInt(number);
+    } else {
+        energyBar.value += parseInt(number);
+    }
+
 }
